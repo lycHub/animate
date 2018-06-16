a325494c70a107d296d27cfb944561c39f6ae08f
lycHub <1120805364@qq.com>
2018-06-16 15:28:28 +0800

    结束

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,21 @@
 import 'style/index.scss';
 
 $(function () {
-  $(document).on('readystatechange', function () {  // 页面加载状态变化时触发
-    // 页面加载状态
-    if (this.readyState === 'complete') {
-      console.log('加载ok');
-      $('.overlay').fadeOut();
+  const imgs = $('img');
+  const len = imgs.length;
+  let num = 0;
+
+  $.each(imgs, function (i) {
+    const oImg = new Image();
+
+    oImg.onload = function () {
+      // 避免重复请求
+      oImg.onload = null;
+      num++;
+      $('.overlay .loading b').text(Number.parseInt(num/len*100) + '%');
+      if (num >= i) $('.overlay').fadeOut();
     }
-  });
+
+    oImg.src = imgs[i].src;
+  })
 })
