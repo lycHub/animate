a19bca79882c2b0d24a1d9acd618a83592120e17
lycHub <1120805364@qq.com>
2018-06-18 14:20:45 +0800

    多页面配置

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,18 @@ const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
   mode: process.env.NODE_ENV || 'production',
   target: 'web',
-  entry: path.join(__dirname, 'src/js/index.js'),
+  entry: {
+    index: path.join(__dirname, 'src/js/index.js'),
+    a: path.join(__dirname, 'src/js/a.js'),
+    b: path.join(__dirname, 'src/js/b.js'),
+    c: path.join(__dirname, 'src/js/c.js')
+  },
   output: {
-    filename: 'js/bundle.[hash:8].js',
+    // chunk可以理解为一个块，即entry中对应的入口
+    // hash指本次打包的hash值，那么所有输出的hash值都一样
+    // chunkhash指本次打包，给每个入口都分配不同的hash值输出
+    // 只有当文件内容改变后，chunkhash才会变化
+    filename: 'js/[name]-[hash:8].bundle.js',
     path: path.join(__dirname, 'dist')
 
     // 默认情况资源文件的请求都会引用相对路径，比如：<script src="js/aaa.js"></script>
@@ -67,14 +76,6 @@ module.exports = {
           }
         },
         'image-webpack-loader'
-        /*{
-          loader: 'url-loader',
-          options: {
-            // limit: 1024,
-            // 静态资源生成的文件目录,与原目录路径统一
-            name: 'assets/[path][name]-[hash:8].[ext]'
-          }
-        }*/
       ]
     }, {
       // 在html中加载图片
@@ -99,14 +100,35 @@ module.exports = {
 
       // 压缩配置 https://github.com/kangax/html-minifier#options-quick-reference
       minify: {
-        removeComments,                         // 删注释
         collapseInlineTagWhitespace: true       // 删空格
       }
 
       // 缓存文件默认true  cache: true
-
-
       // 所有的javascript脚本默认添加在body标签最后面，可以通过inject: 'head'改变
+    }),
+
+    new HTMLPlugin({
+      template: 'src/a.html',
+      filename: 'a-[hash:8].html',
+
+      // 指定该模板依赖的js
+      // 相当于直接把src/js/index.js和src/js/a.js放入body最后面(依据inject)
+      chunks: ['index', 'a']
+
+      // 如果要依赖很多的chunk，可以用反向派出法
+      // excludeChunks: ['c'], 表示除了c，都需要依赖
+    }),
+
+    new HTMLPlugin({
+      template: 'src/b.html',
+      filename: 'b-[hash:8].html',
+      chunks: ['b']
+    }),
+
+    new HTMLPlugin({
+      template: 'src/c.html',
+      filename: 'c-[hash:8].html',
+      chunks: ['c']
     })
   ]
 }
