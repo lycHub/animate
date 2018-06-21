6b6654c5d0915c27225a8ac2ed25a78a6cc0a2e9
lycHub <1120805364@qq.com>
2018-06-21 14:09:31 +0800

    以后拿better-scroll试下吧...

--- src/js/index.js
+++ src/js/index.js
@@ -34,7 +34,7 @@ $(function () {
       this.data = data;
       this.init();
       this.renderDom();
-      // this.bindEvent();
+      this.bindEvent();
     }
 
     init(){
@@ -46,6 +46,9 @@ $(function () {
 
       // 当前索引
       this.idx = 0;
+
+      // 是否可以滑动
+      this.canSlide = false;
     }
 
 
@@ -57,17 +60,63 @@ $(function () {
       const dis = this.slideDis;
 
       this.outer = document.createElement('ul');
+      // this.outer.style.width = `${len * 100}%`;
+      $(this.outer).width(`${len * 100}%`).attr('class', 'clearfix');
 
       for (let i = 0; i < len; i++) {
         const li = document.createElement('li');
         const item = data[i];
-       //  li.style.width = dis + 'px';
-        li.style.transform = `translateX(${dis * i}px)`;
+        li.style.width = dis + 'px';
+        // li.style.transform = `translateX(${dis * i}px)`;
 
         li.innerHTML = `<img src="${item.src}" />`;
         this.outer.appendChild(li);
       }
       wrap.appendChild(this.outer);
+    }
+
+
+    bindEvent(){
+      const self = this;
+      const dis = this.slideDis;
+      const outer = this.outer;
+      const len = this.data.length;
+      // console.log(outer);
+      $(outer).on('touchstart', {context: self}, Slider.onTouchStart)
+        .on('touchmove',  {context: self}, Slider.onTouchMove)
+        .on('touchend',  {context: self}, Slider.onTouchEnd);
+    }
+
+    static onTouchStart(evt) {
+      const self = evt.data.context;
+      self.canSlide = true;
+
+      self.startX = evt.touches[0].pageX;
+
+      // 偏移量
+      self.disX = 0;
+      self.startTime = new Date().getTime();
+    }
+
+    static onTouchMove(evt) {
+      evt.preventDefault();
+      const self = evt.data.context;
+
+      if (self.canSlide) {
+        // <0是左
+        self.disX += evt.touches[0].pageX - self.startX;
+        // console.log(self.disX);
+
+        // 偏移后的位置
+
+        $(self.outer).css('transform', `translateX(${self.disX}px)`);
+      }
+    }
+
+    static onTouchEnd(evt) {
+      const self = evt.data.context;
+      self.canSlide = false;
+      console.log(self.disX);
     }
   }
 
