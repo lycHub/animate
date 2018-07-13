7a60b5944e9ade88d23fc98863a3e652f3f08786
lycHub <1120805364@qq.com>
2018-07-13 20:19:03 +0800

    初始化

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
 import 'style/index.scss';
+import BScroll from 'better-scroll';
 
-console.log(1);
+$(function () {
+  const dataArr = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+  const listWrap = $('.list-wrapper')[0];
+
+  dataArr.forEach((item, key, arr) => {
+    $('.list-content').append(`<li class="list-item">I am the No.${item} line</li>`);
+  });
+
+  setTimeout(() => {
+    const myScroll = new BScroll(listWrap);
+  }, 100);
+});
